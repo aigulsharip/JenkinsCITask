3437fdf0ca4f124c0c0d4622afda796ff4c1c0b8
aigulsharip <aigul.sharip@nu.edu.kz>
2023-12-19 15:15:59 +0600

    Initial commit

---
+++ src/main/java/com/example/jenkinscitask/JenkinsCiTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinscitask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCiTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsCiTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkinscitask/JenkinsCiTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinscitask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsCiTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
